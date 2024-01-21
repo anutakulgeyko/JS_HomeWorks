98dc9eeb5f80ae8beed87681426d3e9a5fbf5f7b
anutakulgeyko <anmi_@ukr.net>
2024-01-21 19:24:55 +0200

    hw13

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
-let userName= prompt('What is your name?');
-console.log(userName);
-alert("Hello, John! How are you?");
+
+const a = +prompt('Введите первое число');
+const b = +prompt('Введите второе число');
+const get = +prompt('What do you want do: +, -, *, /?');
+
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    if (b === 0) {
+        return 'Error';
+    }
+    else {
+        return a / b;
+    }
+}
+
+console.log(a, b);
+console.log(sum(a, b), sub(a, b), mul(a, b), div(a, b));
