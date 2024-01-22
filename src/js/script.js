d13b3e9652f94073379bd9b1ab2a6a9b5f98d3f4
anutakulgeyko <anmi_@ukr.net>
2024-01-23 01:51:10 +0200

    hw15

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
-let userName= prompt('What is your name?');
-console.log(userName);
-alert("Hello, John! How are you?");
+const num1= +prompt('Введите первое число');
+const num2= +prompt('Введите второе число');
+const num3= +prompt('Введите третье число');
+let x = (num1+num2+num3)/3;
+alert(x);
+
+console.log(num1, num2, num3, x);
