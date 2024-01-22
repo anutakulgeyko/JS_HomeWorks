4ae49a82e69467525d33dab0366dcac1213587be
anutakulgeyko <anmi_@ukr.net>
2024-01-23 01:15:10 +0200

    hw14

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,25 @@
 'use strict';
 
-let userName= prompt('What is your name?');
-console.log(userName);
-alert("Hello, John! How are you?");
+// Первый вариант расчета секунд
+const hours = parseInt(prompt('Введите количество часов'));
+const seconds = hours * 3600;
+
+console.log('В ' + hours + ' часах ' + seconds + ' секунд');
+
+
+//Второй вариант, но не получается вывести получ. рез.в алерт(((
+
+const hours = +prompt('Введите количество часов');
+const seconds = hours * 3600;
+function calc (hours, seconds)  {
+    const i = +hours;
+        if (i >= 0 && i <=11) {
+            return(alert(hours));
+        }
+        alert(seconds);
+    }
+
+console.log(hours, seconds);
+
+
+
